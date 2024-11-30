9b3b2e27df28bc16d47395401a5b2e0c5b1b1bda
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2024-11-30 11:18:24 +0700

    new.js

--- pttk.js
+++ pttk.js
@@ -1,18 +1,36 @@
-// Smooth scrolling when clicking on navigation links
-document.querySelectorAll('.nav-link').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Ngừng hành động mặc định (cuộn)
-        
-        // Cuộn mượt đến phần tương ứng
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth' // Thiết lập hiệu ứng cuộn mượt
-        });
-    });
-});
+console.log("hello");
+/* Khai báo biến let , var , const */
 
-// Cập nhật hành động thanh toán khi nhấn nút "Thanh Toán"
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('checkoutButton').addEventListener('click', function () {
-        window.location.href = 'payment.html'; // Chuyển hướng đến trang thanh toán (ví dụ payment.html)
-    });
-});
+let a = 7;
+console.log(`Gia tri ban dau a = ${a}`);
+ 
+a = a + 3 ;
+console.log(`Gia tri sau khi thay doi a = ${a}`);
+
+/* String */
+let namee = "BachAn";
+console.log(namee.at(3))
+console.log(`Gia tri ban dau ${namee} , sau khi dung namee.toLowerCase() = `)
+console.log(namee.toLowerCase()) 
+console.log(namee.endsWith("a")) /* false */
+
+/* reguler express trong tri tue nhan tao */
+
+console.log(namee.slice(2, 4)) ;/* an */ /* Cắt từ vị trí thứ 2 đến thứ 4 */ 
+console.log(namee.substring(1,2)); 
+
+age = 19;
+console.log(age++); /* 19 */
+console.log(age); /* 20 */
+console.log(++age); /* 21 */
+
+n = 3 /* 0011 */
+console.log(n<<1)  /* 0 0110 = 6 */
+
+n = 23 /* 0001  */
+console.log(n<<1) /* 14 */
+
+/* Nhập vào từ người dùng và in ra 1 nội dung nào đó */
+let weight = prompt("Nhap vao can nang cua ban : ")
+let height = prompt("Nhap vao chieu cao cua ban : ")
+console.log(`Chi so BMI cua ban la : ${height/weight}`)
